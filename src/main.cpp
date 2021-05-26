4a7d4bb02c44f51f6fbacf81e13eb018d7bc609f
andrey <andre547481@gmail.com>
2021-05-26 17:10:44 +0300

    пустой проект
    	новый файл:    .github/workflows/release.yml
    	новый файл:    CMakeLists.txt
    	новый файл:    src/lib.cpp
    	новый файл:    src/lib.h
    	новый файл:    src/main.cpp
    	новый файл:    src/test_version.cpp
    	новый файл:    version.h.in

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "lib.h"
+
+#define LOG_ALLOC
+#define LOG_LIST
+
+#include <iostream>
+// #include <fstream>
+// #include <sstream>
+#include <map>
+#include <vector>
+// #include <iterator>
+// #include <algorithm>
+
+
+void testCommon()
+{
+
+}
+
+
+
+
+int main (int, char **)
+{
+    
+    std::cout << "Design Version: " << version() << std::endl;
+
+    // testCommon();
+
+    return 0;
+}
