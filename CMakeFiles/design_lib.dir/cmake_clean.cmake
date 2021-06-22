e729ddcc14da7760750446988baa8a6aef1e4289
AndreyPrimary <AndreyPrimary@users.noreply.github.com>
2021-06-22 20:38:31 +0000

    deploy: 8bb001c5561d61514f689949b77a0e3f50fad33e

---
+++ CMakeFiles/design_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design_lib.dir/src/lib.cpp.o"
+  "CMakeFiles/design_lib.dir/src/lib.cpp.o.d"
+  "libdesign_lib.a"
+  "libdesign_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/design_lib.dir/src/lib.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/design_lib.dir/src/lib.cpp.o: \
+ /home/runner/work/03_home/03_home/src/lib.cpp /usr/include/stdc-predef.h \
+ /home/runner/work/03_home/03_home/src/lib.h \
+ /home/runner/work/03_home/03_home/src/version.h
