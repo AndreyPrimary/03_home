04f27647e75c03d8fc7685b7b31910080f098a8b
AndreyPrimary <AndreyPrimary@users.noreply.github.com>
2021-06-22 19:48:23 +0000

    deploy: 14f18f1ae40a1d58a68fd1d63ba4d8542c2208dc

--- CMakeFiles/design_lib.dir/src/lib.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/design_lib.dir/src/lib.cpp.o: \
- /home/runner/work/03_home/03_home/src/lib.cpp /usr/include/stdc-predef.h \
- /home/runner/work/03_home/03_home/src/lib.h \
- /home/runner/work/03_home/03_home/src/version.h
